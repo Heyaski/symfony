cde2c668080e460060c0070306db1794454d9f01
Stepan <stavchig@mail.ru>
2024-10-19 15:41:32 +1000

    third task

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -1,51 +1,81 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
-use App\Service\StockService;
+use App\Entity\Stock;
+use App\Form\StockType;
+use App\Repository\StockRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class StockController extends AbstractController
+#[Route('/stock')]
+final class StockController extends AbstractController
 {
-    public function __construct(private readonly StockService $stockService)
+    #[Route(name: 'app_stock_index', methods: ['GET'])]
+    public function index(StockRepository $stockRepository): Response
     {
-
+        return $this->render('stock/index.html.twig', [
+            'stocks' => $stockRepository->findAll(),
+        ]);
     }
 
-    #[Route(path: '/hello')]
-    public function index(): Response
+    #[Route('/new', name: 'app_stock_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return new Response('Hello World!');
+        $stock = new Stock();
+        $form = $this->createForm(StockType::class, $stock);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($stock);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stock_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('stock/new.html.twig', [
+            'stock' => $stock,
+            'form' => $form,
+        ]);
     }
 
-    #[Route('/hello/{name}')]
-    public function getHelloName(string $name): Response
+    #[Route('/{id}', name: 'app_stock_show', methods: ['GET'])]
+    public function show(Stock $stock): Response
     {
-        return new Response('Hello ' . $name);
+        return $this->render('stock/show.html.twig', [
+            'stock' => $stock,
+        ]);
     }
 
-    #[Route(path: '/game/guess/{number}/{name}', methods: ['GET'])]
-    public function guessGame(int $number, string $name): Response
+    #[Route('/{id}/edit', name: 'app_stock_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Stock $stock, EntityManagerInterface $entityManager): Response
     {
-        $randomNumber = $this->stockService->getRandomNumber();
-		$randomName = $this->stockService->getAuthorName();
+        $form = $this->createForm(StockType::class, $stock);
+        $form->handleRequest($request);
 
-		if ($randomNumber === $number && $randomName === $name) {
-			return new Response('Congratulates, you guess number and name');
-		}
-		elseif ($randomNumber === $number) {
-			return new Response("Number guess but name not");
-		}
-		elseif ($randomName === $name) {
-			return new Response("Name guess but number not");
-		}
-		else {
-			return new Response("Try again!");
-		} 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stock_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('stock/edit.html.twig', [
+            'stock' => $stock,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_stock_delete', methods: ['POST'])]
+    public function delete(Request $request, Stock $stock, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$stock->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($stock);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_stock_index', [], Response::HTTP_SEE_OTHER);
     }
 }
